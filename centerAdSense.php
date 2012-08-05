c17195f55b2e1f3ece387a01e8b28188d1c6b5ca
Kenji Ohtsuka <kok.fdcm@gmail.com>
2012-08-04 18:14:44 -0700

    first commit

---
+++ centerAdSense.php
@@ -0,0 +1,26 @@
+<?php
+echo <<<EOT
+<div style="text-align:center;width:728px;margin:0 auto;"><script type="text/javascript"><!--
+google_ad_client = "ca-pub-XXXX";
+/* TeXBBSLink */
+google_ad_slot = "XXXX";
+google_ad_width = 728;
+google_ad_height = 15;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-XXXX";
+/* BBScenter */
+google_ad_slot = "XXXX";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></div>
+EOT;
+?>
