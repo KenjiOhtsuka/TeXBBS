b522fd9856cf7a347e71850bf714d8b3524a4c32
Kenji Ohtsuka <kok.fdcm@gmail.com>
2013-10-27 22:55:49 +0900

    githubにプッシュするため修正

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-  $DB = "MyNippon";// 'a5697723_AIOdb';
+  $DB = "db_name";
   $myCon = mysql_connect(
-    'localhost', //'XXXX',
-    'root', //'XXXX',
-    'XXXX' //'XXXX'
+    'localhost',
+    'root', 
+    'password'
   );
   if ($myCon == false) die("Connection failed.");
   if (!mysql_select_db($DB, $myCon)) die("Selection failed.");
